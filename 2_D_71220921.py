1c5920e2336fcf9285c22f5b42328a38984147a9
AlberthWatori <117901024+AlberthWatori@users.noreply.github.com>
2022-12-01 16:58:09 +0700

    Add files via upload

---
+++ 2_D_71220921.py
@@ -0,0 +1,10 @@
+def cekhuruf () :
+    kata=(input("masukan kata :"))
+    list=(kata)
+    len(kata)
+    if(len(kata)%2==0) :
+        print("huruf ujung pada kata",kata, "adalah",(kata{-3:}))
+    else :
+        print("huruf paling ujung pada kata",kata,"adalah",(kata{:3}))
+    return (kata)
+cetakHuruf()
